3106b1f971ebf8bd9ab8210c1c6803a456c97193
JThomas28 <jonathan.thomas.14@cnu.edu>
2017-02-15 22:23:28 -0500

    File /Users/JonathanThomas/Documents/Senior Year/Spring 2017/Capstone/Ring Counter/src/TestClass.java changed at Wed Feb 15 22:23:28 EST 2017

---
+++ src/TestClass.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @author JonathanThomas
+ * @version 2/15/16
+ * This is an empty class used to test pushing to remote repository
+ */
+public class TestClass {
+	public static void main(String [] args)
+	{
+		System.out.println("Repository Tester!");
+	}
+}
