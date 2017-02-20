7a7e628e7252c6ad573f2c7a797bb223502a13b0
JThomas28 <jonathan.thomas.14@cnu.edu>
2017-02-20 16:15:16 -0500

    File /Users/JonathanThomas/Documents/Senior Year/Spring 2017/Capstone/Ring Counter/src/Constants.java changed at Mon Feb 20 16:15:16 EST 2017

--- src/Constants.java
+++ src/Constants.java
@@ -3,5 +3,5 @@ public interface Constants
 	public static final String FRAME_TITLE = "Ring Counter"; 
 	public static final String CHOOSE_IMAGE_TEXT = "Choose Image To Upload";
 	public static final String AVAILABLE_FILE_FORMATS = "Available file formats include:";
-	public static final String PATH_TO_BACKGROUND_IMAGE = "forest-07.jpg";
+	public static final String PATH_TO_BACKGROUND_IMAGE = "forestBackground.jpg";
 }
