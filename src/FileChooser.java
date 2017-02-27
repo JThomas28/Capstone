9cc6705be3dd960ea4fc39c7cd4680c36c4340f0
JThomas28 <jonathan.thomas.14@cnu.edu>
2017-02-27 17:40:57 -0500

    File /Users/JonathanThomas/Documents/Senior Year/Spring 2017/Capstone/Ring Counter/src/FileChooser.java changed at Mon Feb 27 17:40:57 EST 2017

--- src/FileChooser.java
+++ src/FileChooser.java
@@ -1,36 +1,3 @@
-/*
- * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- *   - Neither the name of Oracle or the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-//package components;
-
 import java.io.*;
 import java.awt.*;
 import javax.swing.*;
@@ -39,7 +6,6 @@ import javax.swing.SwingUtilities;
 @SuppressWarnings("serial")
 public class FileChooser extends JPanel
 {
-	static private final String newline = "\n";
 	JTextArea log;
 	JFileChooser fc;
 	private static File chosenFile;
@@ -70,11 +36,6 @@ public class FileChooser extends JPanel
 			// This is where a real application would open the file.
 			//log.append("Opening: " + file.getName() + "." + newline);
 		}
-		//else
-		//{
-			//log.append("Open command cancelled by user." + newline);
-		//}
-		//log.setCaretPosition(log.getDocument().getLength());
 	}
 
 	/** Returns an ImageIcon, or null if the path was invalid. */
@@ -92,25 +53,12 @@ public class FileChooser extends JPanel
 		}
 	}
 
-	/**
-	 * Create the GUI and show it. For thread safety, this method should be
-	 * invoked from the event dispatch thread.
-	 */
 	private static void createAndShowGUI()
 	{
 		// Create and set up the window.
 		JPanel pane = new JPanel();
 		pane.add(new FileChooser());
 		pane.setVisible(true);
-//		JFrame frame = new JFrame("FileChooser");
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//		// Add content to the window.
-//		frame.add(new FileChooser());
-//
-//		// Display the window.
-//		frame.pack();
-//		frame.setVisible(true);
 	}
 
 	public static File getFile()
