73d57323a77fb25d1b0e8dae44d79b2414f132f1
JThomas28 <jonathan.thomas.14@cnu.edu>
2017-02-27 16:55:16 -0500

    File /Users/JonathanThomas/Documents/Senior Year/Spring 2017/Capstone/Ring Counter/src/GUI.java changed at Mon Feb 27 16:53:49 EST 2017

--- src/GUI.java
+++ src/GUI.java
@@ -85,10 +85,7 @@ public class GUI extends JFrame implements Constants
 	
 	public void secondGUI()
 	{
-		getContentPane().setLayout(new GridLayout(3, 1));
-		//picture first, estimated age, more info button
-		JButton moreInfoButton = new JButton(MORE_INFO);
-		add(moreInfoButton);
+		
 	}
 
 	public GUI()
