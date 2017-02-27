a9dfcd8d9a5365afe9a5abbfc0956fb3f672dc41
JThomas28 <jonathan.thomas.14@cnu.edu>
2017-02-27 15:29:23 -0500

    File /Users/JonathanThomas/Documents/Senior Year/Spring 2017/Capstone/Ring Counter/src/GUI.java changed at Mon Feb 27 15:29:23 EST 2017

--- src/GUI.java
+++ src/GUI.java
@@ -15,46 +15,45 @@ public class GUI extends JFrame implements Constants
 	private class GUIPanel extends JPanel
 	{
 		private Image image;
-		
-		//getImageFilePath
+
+		// getImageFilePath
 		public GUIPanel(String imgString) throws IOException
 		{
 			image = ImageIO.read(new File(imgString));
 		}
-		
+
 		public void paintComponent(Graphics g)
 		{
 			super.paintComponent(g);
 			g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
 		}
-		
+
 		public GUIPanel()
 		{
 			setLayout(new FlowLayout());
 			setPreferredSize(new Dimension(250, 250));
 			setBackground(Color.BLUE);
-			
-			
+
 			JLabel chooseFileText = new JLabel();
 			JButton uploadButton = new JButton();
 			JLabel fileFormatsAllowed = new JLabel(AVAILABLE_FILE_FORMATS);
 
 			chooseFileText.setText(CHOOSE_IMAGE_TEXT);
-			uploadButton.setText(BROWSE);//CHOOSE_IMAGE_TEXT);
+			uploadButton.setText(BROWSE);// CHOOSE_IMAGE_TEXT);
 			uploadButton.addActionListener(new ActionListener()
 			{
 				@Override
 				public void actionPerformed(ActionEvent e)
 				{
-					//open file explorer
-//					try
-//					{
-//						Desktop.getDesktop().open(imageFile);
-//					}
-//					catch (IOException e1)
-//					{
-//						e1.printStackTrace();
-//					}
+					// open file explorer
+					// try
+					// {
+					// Desktop.getDesktop().open(imageFile);
+					// }
+					// catch (IOException e1)
+					// {
+					// e1.printStackTrace();
+					// }
 				}
 			});
 			add(uploadButton);
@@ -68,27 +67,27 @@ public class GUI extends JFrame implements Constants
 	{
 		super(FRAME_TITLE);// title
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		
+
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		setSize(screenSize);
-		
-		Container contentPane = getContentPane();
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 0,90));
-		
-		JPanel panel = new GUIPanel();
-		add(panel);
-		
-//		JPanel panel;
-//		//draw background image.
-//		try
-//		{
-//			panel = new GUIPanel(PATH_TO_BACKGROUND_IMAGE);
-//			getContentPane().add(panel);
-//		}
-//		catch (IOException e)
-//		{
-//			e.printStackTrace();
-//		}
+
+		// Container contentPane = getContentPane();
+		// contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 90));
+
+		// JPanel panel = new GUIPanel();
+		// getContentPane().add(panel);
+
+		JPanel panel;
+		// draw background image.
+		try
+		{
+			panel = new GUIPanel(PATH_TO_BACKGROUND_IMAGE);
+			getContentPane().add(panel);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args)
