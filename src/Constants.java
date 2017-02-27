baae95f05123e80721304aec3ce3d07053547428
JThomas28 <jonathan.thomas.14@cnu.edu>
2017-02-27 15:55:29 -0500

    File /Users/JonathanThomas/Documents/Senior Year/Spring 2017/Capstone/Ring Counter/src/Constants.java changed at Mon Feb 27 15:55:28 EST 2017

--- src/Constants.java
+++ src/Constants.java
@@ -5,5 +5,5 @@ public interface Constants
 	public static final String AVAILABLE_FILE_FORMATS = "Available file formats include:";
 	public static final String PATH_TO_BACKGROUND_IMAGE = "forestBackground.jpg";
 	public static final String BROWSE = "Browse...";
-	public static final String ZIPCODE_TEXT = "Enter Zipcode where tree was found";
+	public static final String ZIPCODE_TEXT = "Enter Zipcode where tree was found:";
 }
