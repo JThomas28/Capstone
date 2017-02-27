117d3f960cf51f4ea21bc3b647bd5faa1aa018d1
JThomas28 <jonathan.thomas.14@cnu.edu>
2017-02-27 17:36:28 -0500

    File /Users/JonathanThomas/Documents/Senior Year/Spring 2017/Capstone/Ring Counter/src/FileChooser.java changed at Mon Feb 27 17:36:28 EST 2017

--- src/FileChooser.java
+++ src/FileChooser.java
@@ -27,154 +27,106 @@
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */ 
+ */
 
 //package components;
 
 import java.io.*;
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.SwingUtilities;
 
 @SuppressWarnings("serial")
 public class FileChooser extends JPanel
-                             implements ActionListener {
-    static private final String newline = "\n";
-    JButton openButton, saveButton;
-    JTextArea log;
-    JFileChooser fc;
-    private static File chosenFile;
+{
+	static private final String newline = "\n";
+	JTextArea log;
+	JFileChooser fc;
+	private static File chosenFile;
 
-    public FileChooser() {
-        super(new BorderLayout());
+	public FileChooser()
+	{
+		super(new BorderLayout());
 
-        //Create the log first, because the action listeners
-        //need to refer to it.
-        log = new JTextArea(5,20);
-        log.setMargin(new Insets(5,5,5,5));
-        log.setEditable(false);
-        JScrollPane logScrollPane = new JScrollPane(log);
+		// Create a file chooser
+		fc = new JFileChooser();
 
-        //Create a file chooser
-        fc = new JFileChooser();
+		// Uncomment one of the following lines to try a different
+		// file selection mode. The first allows just directories
+		// to be selected (and, at least in the Java look and feel,
+		// shown). The second allows both files and directories
+		// to be selected. If you leave these lines commented out,
+		// then the default mode (FILES_ONLY) will be used.
+		//
+		// fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		
+		int returnVal = fc.showOpenDialog(FileChooser.this);
 
-        //Uncomment one of the following lines to try a different
-        //file selection mode.  The first allows just directories
-        //to be selected (and, at least in the Java look and feel,
-        //shown).  The second allows both files and directories
-        //to be selected.  If you leave these lines commented out,
-        //then the default mode (FILES_ONLY) will be used.
-        //
-        //fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		if (returnVal == JFileChooser.APPROVE_OPTION)
+		{
+			File file = fc.getSelectedFile();
+			chosenFile = file;
+			// This is where a real application would open the file.
+			//log.append("Opening: " + file.getName() + "." + newline);
+		}
+		//else
+		//{
+			//log.append("Open command cancelled by user." + newline);
+		//}
+		//log.setCaretPosition(log.getDocument().getLength());
+	}
 
-        //Create the open button.  We use the image from the JLF
-        //Graphics Repository (but we extracted it from the jar).
-//        openButton = new JButton("Open a File...");//,
-//                                 //createImageIcon("images/Open16.gif"));
-//        openButton.addActionListener(this);
+	/** Returns an ImageIcon, or null if the path was invalid. */
+	protected static ImageIcon createImageIcon(String path)
+	{
+		java.net.URL imgURL = FileChooser.class.getResource(path);
+		if (imgURL != null)
+		{
+			return new ImageIcon(imgURL);
+		}
+		else
+		{
+			System.err.println("Couldn't find file: " + path);
+			return null;
+		}
+	}
 
-        //Create the save button.  We use the image from the JLF
-        //Graphics Repository (but we extracted it from the jar).
-//        saveButton = new JButton("Save a File...",
-//                                 createImageIcon("images/Save16.gif"));
-//        saveButton.addActionListener(this);
+	/**
+	 * Create the GUI and show it. For thread safety, this method should be
+	 * invoked from the event dispatch thread.
+	 */
+	private static void createAndShowGUI()
+	{
+		// Create and set up the window.
+		JFrame frame = new JFrame("FileChooser");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //For layout purposes, put the buttons in a separate panel
-//        JPanel buttonPanel = new JPanel(); //use FlowLayout
-//        buttonPanel.add(openButton);
-        //buttonPanel.add(saveButton);
+		// Add content to the window.
+		frame.add(new FileChooser());
 
-        //Add the buttons and the log to this panel.
-        //add(buttonPanel, BorderLayout.PAGE_START);
-        add(logScrollPane, BorderLayout.CENTER);
-        int returnVal = fc.showOpenDialog(FileChooser.this);
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
 
-        if (returnVal == JFileChooser.APPROVE_OPTION) {
-            File file = fc.getSelectedFile();
-            chosenFile = file;
-            //This is where a real application would open the file.
-            log.append("Opening: " + file.getName() + "." + newline);
-        } else {
-            log.append("Open command cancelled by user." + newline);
-        }
-        log.setCaretPosition(log.getDocument().getLength());
-    }
+	public static File getFile()
+	{
+		return chosenFile;
+	}
 
-//    public void actionPerformed(ActionEvent e) {
-//
-//        //Handle open button action.
-//        if (e.getSource() == openButton) {
-//            int returnVal = fc.showOpenDialog(FileChooser.this);
-//
-//            if (returnVal == JFileChooser.APPROVE_OPTION) {
-//                File file = fc.getSelectedFile();
-//                chosenFile = file;
-//                //This is where a real application would open the file.
-//                log.append("Opening: " + file.getName() + "." + newline);
-//            } else {
-//                log.append("Open command cancelled by user." + newline);
-//            }
-//            log.setCaretPosition(log.getDocument().getLength());
-//
-//        //Handle save button action.
-//        } else if (e.getSource() == saveButton) {
-//            int returnVal = fc.showSaveDialog(FileChooser.this);
-//            if (returnVal == JFileChooser.APPROVE_OPTION) {
-//                File file = fc.getSelectedFile();
-//                //This is where a real application would save the file.
-//                log.append("Saving: " + file.getName() + "." + newline);
-//            } else {
-//                log.append("Save command cancelled by user." + newline);
-//            }
-//            log.setCaretPosition(log.getDocument().getLength());
-//        }
-//    }
-
-    /** Returns an ImageIcon, or null if the path was invalid. */
-    protected static ImageIcon createImageIcon(String path) {
-        java.net.URL imgURL = FileChooser.class.getResource(path);
-        if (imgURL != null) {
-            return new ImageIcon(imgURL);
-        } else {
-            System.err.println("Couldn't find file: " + path);
-            return null;
-        }
-    }
-
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event dispatch thread.
-     */
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("FileChooserDemo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //Add content to the window.
-        frame.add(new FileChooser());
-
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
-    
-    public static File getFile()
-    {
-    	return chosenFile;
-    }
-
-    public static void main(String[] args) {
-        //Schedule a job for the event dispatch thread:
-        //creating and showing this application's GUI.
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                //Turn off metal's use of bold fonts
-                UIManager.put("swing.boldMetal", Boolean.FALSE); 
-                createAndShowGUI();
-            }
-        });
-    }
+	public static void main(String[] args)
+	{
+		// Schedule a job for the event dispatch thread:
+		// creating and showing this application's GUI.
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				// Turn off metal's use of bold fonts
+				UIManager.put("swing.boldMetal", Boolean.FALSE);
+				createAndShowGUI();
+			}
+		});
+	}
 }
