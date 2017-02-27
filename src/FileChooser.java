bd6fa3587ab4809158522b3a4501c56c97d2ace8
JThomas28 <jonathan.thomas.14@cnu.edu>
2017-02-27 17:18:26 -0500

    File /Users/JonathanThomas/Documents/Senior Year/Spring 2017/Capstone/Ring Counter/src/FileChooser.java changed at Mon Feb 27 17:18:26 EST 2017

--- src/FileChooser.java
+++ src/FileChooser.java
@@ -75,20 +75,20 @@ public class FileChooser extends JPanel
 
         //Create the open button.  We use the image from the JLF
         //Graphics Repository (but we extracted it from the jar).
-        openButton = new JButton("Open a File...",
-                                 createImageIcon("images/Open16.gif"));
+        openButton = new JButton("Open a File...");//,
+                                 //createImageIcon("images/Open16.gif"));
         openButton.addActionListener(this);
 
         //Create the save button.  We use the image from the JLF
         //Graphics Repository (but we extracted it from the jar).
-        saveButton = new JButton("Save a File...",
-                                 createImageIcon("images/Save16.gif"));
-        saveButton.addActionListener(this);
+//        saveButton = new JButton("Save a File...",
+//                                 createImageIcon("images/Save16.gif"));
+//        saveButton.addActionListener(this);
 
         //For layout purposes, put the buttons in a separate panel
         JPanel buttonPanel = new JPanel(); //use FlowLayout
         buttonPanel.add(openButton);
-        buttonPanel.add(saveButton);
+        //buttonPanel.add(saveButton);
 
         //Add the buttons and the log to this panel.
         add(buttonPanel, BorderLayout.PAGE_START);
