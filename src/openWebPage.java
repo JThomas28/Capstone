487f64e5004dd17911e2d3f6ebde2060b6ee4427
JThomas28 <jonathan.thomas.14@cnu.edu>
2017-02-28 11:03:21 -0500

    File /Users/JonathanThomas/Documents/Senior Year/Spring 2017/Capstone/Ring Counter/src/openWebPage.java changed at Tue Feb 28 11:03:20 EST 2017

---
+++ src/openWebPage.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+import javax.swing.*;
+
+public class openWebPage
+{
+	public static void main(String [] args) throws IOException
+	{
+		JEditorPane webpage = new JEditorPane("https://www.wunderground.com/history/");
+		
+		JFrame webpageFrame = new JFrame("Weather History");
+		webpageFrame.add(new JScrollPane(webpage));
+		
+		webpageFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		webpageFrame.setVisible(true);
+		webpageFrame.pack();
+	}
+}
